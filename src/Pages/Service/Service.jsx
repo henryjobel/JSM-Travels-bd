e6066717ffc42420f23a797b1f9ad053f21297f2
henryjobel <jhobelsaha@gmail.com>
2023-12-24 12:13:49 +0600

    banner and service page created

--- src/Pages/Service/Service.jsx
+++ src/Pages/Service/Service.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+
 
 const Service = () => {
     return (
         <div>
-            <h1>service</h1>
+            <div className="hero h-[800px]" style={{ backgroundImage: 'url(https://i.ibb.co/XpN1h7c/pictr.jpg)' }}>
+                <div className="hero-overlay bg-opacity-40"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className='gap-7 flex'>
+                       <button className='btn  text-white bg-blue-700 bg-opacity-30 hover:bg-blue-700 text-xl'>Air Ticket</button>
+                       <button className='btn  text-white bg-blue-700 bg-opacity-30 hover:bg-blue-700 text-xl'>Train Tickets</button>
+                       <button className='btn  text-white bg-blue-700 bg-opacity-30 hover:bg-blue-700 text-xl'>Bus Tickets</button>
+                       <button className='btn  text-white bg-blue-700 bg-opacity-30 hover:bg-blue-700 text-xl'>Tour Package</button>
+                       <button className='btn  text-white bg-blue-700 bg-opacity-30 hover:bg-blue-700 text-xl'>Hotel Booking</button>
+                       <button className='btn  text-white bg-blue-700 bg-opacity-30 hover:bg-blue-700 text-xl'>Visa</button>
+                       <button className='btn  text-white bg-blue-700 bg-opacity-30 hover:bg-blue-700 text-xl'>Medical Tourism</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
